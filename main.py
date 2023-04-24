d5ca6106f8589300b71d2cdc12801afbc7da9c70
MuhHanif <48muhhanif@gmail.com>
2023-04-24 11:00:35 -0700

    api inference

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import nest_asyncio
 from pyngrok import ngrok
 import uvicorn
 import json
+from module.api_module import * 
 
 # read file config
 with open('config_file.json') as f:
@@ -14,4 +15,4 @@ if use_ngrok:
     ngrok_tunnel = ngrok.connect(8000)
     print('Public URL:', ngrok_tunnel.public_url)
     nest_asyncio.apply()
-uvicorn.run(app, port=8000)
+uvicorn.run(app, port=8000, host='0.0.0.0')
--- module/api_module.py
+++ module/api_module.py
@@ -4,6 +4,8 @@ from queue import Queue
 import os
 import configparser
 import json
+import time
+from .inference_module import *
 
 with open('config_file.json') as f:
     config = json.load(f)
@@ -56,3 +58,77 @@ async def upload_audio_file(file: UploadFile = File(...)) -> dict:
     # audio_queue.put(contents)
 
     return output
+
+@app.post("/upload/audio_test_upload/")
+async def upload_audio_file_test_upload(file: UploadFile = File(...)) -> dict:
+    """
+    Endpoint that accepts an audio file and saves it to a specified folder.
+
+    Args:
+        file (UploadFile): The audio file to upload.
+
+    Returns:
+        dict: A JSON object containing the filename of the uploaded file.
+    """
+    # Specify the folder where the file will be saved
+    upload_folder = "/content/uploads"
+
+    # Create the folder if it doesn't exist
+    os.makedirs(upload_folder, exist_ok=True)
+
+    # Check if the uploaded file is an audio file
+    valid_extensions = [".mp3", ".wav", ".ogg"]
+    file_extension = os.path.splitext(file.filename)[1].lower()
+
+    if file_extension not in valid_extensions:
+        raise HTTPException(status_code=400, detail="File must be an audio file")
+
+    # Save the file to the specified folder
+    file_path = os.path.join(upload_folder, file.filename)
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+    
+    
+    # im disabling using queue for now   
+    # contents: bytes = await file.read()
+    # audio_queue.put(contents)
+
+    return {"message": "working as intended"}
+
+@app.post("/upload/audio_test_preprocessing/")
+async def upload_audio_file_test_preprocessing(file: UploadFile = File(...)) -> dict:
+    """
+    Endpoint that accepts an audio file and saves it to a specified folder.
+
+    Args:
+        file (UploadFile): The audio file to upload.
+
+    Returns:
+        dict: A JSON object containing the filename of the uploaded file.
+    """
+    # Specify the folder where the file will be saved
+    upload_folder = "/content/uploads"
+
+    # Create the folder if it doesn't exist
+    os.makedirs(upload_folder, exist_ok=True)
+
+    # Check if the uploaded file is an audio file
+    valid_extensions = [".mp3", ".wav", ".ogg"]
+    file_extension = os.path.splitext(file.filename)[1].lower()
+
+    if file_extension not in valid_extensions:
+        raise HTTPException(status_code=400, detail="File must be an audio file")
+
+    # Save the file to the specified folder
+    file_path = os.path.join(upload_folder, file.filename)
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+    
+    tic = time.time()
+    audio = split_stereo_audio_ffmpeg(file_path)
+    tac = time.time()
+    # im disabling using queue for now   
+    # contents: bytes = await file.read()
+    # audio_queue.put(contents)
+
+    return {"message": f"processing audio took {tac-tic} second(s)"}
