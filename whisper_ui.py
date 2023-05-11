edd8cd6e2dbd93cd96fbcfa07c9e482d71a1a7a6
MuhHanif <48muhhanif@gmail.com>
2023-05-11 11:44:16 -0700

    simple gradio demo sequential

--- whisper_ui.py
+++ whisper_ui.py
@@ -1,19 +1,54 @@
 import gradio as gr
+import json
+from module.inference_module import *
+import os
+
+whisper_config = "config_file.json"
+json_config = "conf.json"
+
+# Get the absolute path to the directory of the current script
+dir_path = os.path.dirname(os.path.realpath(__file__))
+# dir_path = os.path.dirname(dir_path)
+# Define the path to the config file relative to the script directory
+config_path = os.path.join(dir_path, json_config)
+whisper_config_path = os.path.join(dir_path, whisper_config)
+
+# read config file for credentials and queue
+with open(config_path, "r") as conf:
+    config = json.load(conf)
+
+# read config file for whisper models
+with open(whisper_config_path, "r") as whisper_conf:
+    whisper_config = json.load(whisper_conf)
+
+# pin the model into memory
+model = load_model(whisper_config["models"], device=whisper_config["cuda_device"])
 
 
 def transcribe_audio(audio_file):
-    print("+++++++++[testing]+++++++++")
-    text = "testing"
-    return text
+    file_name = audio_file.name
+
+    output = whisper_to_vtt(
+        model,
+        file_name,
+        # TODO! put this output dir in the config
+        output_dir="/kaggle/working/test",
+        channel_uid=[f"{'speaker1'}:", f"{'speaker2'}:"],
+    )
+
+    return output
 
 
-audio_input = gr.inputs.Audio(label="Upload Audio File")
-output_text = gr.outputs.Textbox(label="Transcription")
+# gradio ui input output
+inputs = gr.inputs.File()
+outputs = gr.outputs.Textbox()
 
+
+# start interface and executing defined function
 gr.Interface(
     fn=transcribe_audio,
-    inputs=audio_input,
-    outputs=output_text,
-    title="Audio Transcription",
-    description="Transcribe an uploaded audio file to text.",
-).launch()
+    inputs=inputs,
+    outputs=outputs,
+    title="Whisper Testing Sequential",
+    allow_flagging="never",
+).launch(server_name="0.0.0.0", server_port=9789)
