6d198ca5b11c1fc0fad3f1bf0205159348ce1d0c
MuhHanif <48muhhanif@gmail.com>
2023-05-22 10:23:33 -0700

    ephemeral transcribtion

--- module/api_module.py
+++ module/api_module.py
@@ -9,10 +9,10 @@ from .inference_module import *
 import uuid
 from threading import Thread
 import boto3
-from botocore.exceptions import NoCredentialsError
 from botocore.client import Config
 
 print(os.getcwd())
+# TODO! change this to proper path
 with open("config_file.json") as f:
     config = json.load(f)
 
@@ -20,17 +20,16 @@ with open("minio_creds.json") as creds_file:
     creds = json.load(creds_file)
 
 app = FastAPI()
-# might be faster if using queue instead of storing as a file
-# audio_queue = Queue()
 
 # pin the model into memory
-if config["debug_without_model"]==False:
+if config["debug_without_model"] == False:
     model = load_model(config["models"])
 
 # local data
 
 # TODO! audio_file and srt_file need to be purged periodically
 # create thread function to purge it
+
 audio_file = {}
 internal_queue = Queue(maxsize=config["maximum_queue"])
 
@@ -42,26 +41,23 @@ def upload_to_bucket(file_path: str) -> None:
     Args:
         file_path (str): The local file path of the file to upload.
 
-    Raises:
-        NoCredentialsError: If no AWS credentials are found or they are invalid.
     """
-    
+
     # Specify the profile name from the .aws/credentials file
     # profile_name = 's3fs-minio'
     profile_name = config["bucket_cred_profile"]
 
     # Create a session using the specified profile
     # TODO! cant use profile, pass as args instead
-    session = boto3.session.Session() # boto3.Session(profile_name=profile_name)
+    session = boto3.session.Session()  # boto3.Session(profile_name=profile_name)
 
     # Create an S3 client using the session
     s3_client = session.client(
-        's3',
+        "s3",
         endpoint_url=creds["endpoint"],
         aws_access_key_id=creds["access_key_id"],
         aws_secret_access_key=creds["secret_access_key"],
-        config=Config(signature_version='s3v4'),
-    
+        config=Config(signature_version="s3v4"),
     )
 
     # Specify the bucket name
@@ -74,14 +70,61 @@ def upload_to_bucket(file_path: str) -> None:
         # Specify the local file path and desired S3 key (file name)
         local_file_path = file_path
         s3_key = file_name
-        
+
         # Upload the file to the bucket
         s3_client.upload_file(file_path, bucket_name, s3_key)
         print("File uploaded successfully.")
-    except NoCredentialsError:
-        print("No AWS credentials found or they are invalid.")    
+    except Exception as error_message:
+        print(error_message)
 
     pass
+
+
+def purge_data_periodially():
+    """
+    purge data from local disk and dict after x minutes (epehemeral data) when transcribtion either succeed or failed
+    only check for every 1 minute
+
+    # dictionary changed size during iteration
+    """
+    while True:
+        try:
+            for key, value in audio_file.items():
+                # wow black formatter made a mess
+                # basically it check if transcribtion status is "transcribed"
+                # and it's longer than defined ephemeral duration it will delete
+                # the file to freed up space in the worker machine.
+                if (
+                    value["status"] == "transcribed"
+                    and time.time() - value["timestamp"]
+                    > config["ephemeral_data_duration"]
+                ):
+                    # deleting audio file
+                    os.remove(value["file"])
+
+                    # delete VTT
+                    for file in value["list_file_path"]:
+                        os.remove(file)
+                    # delete dict
+                    del audio_file[key]
+
+                # delete failed transcribtion after x seconds
+                if (
+                    value["status"] == "failed to transcribe audio"
+                    and time.time() - value["timestamp"]
+                    > config["ephemeral_data_duration"]
+                ):
+                    # deleting audio file
+                    os.remove(value["file"])
+                    # delete dict
+                    del audio_file[key]
+
+        except Exception as purging_error:
+            print(purging_error)
+
+        # sleep periodically to not hog resources
+        time.sleep(config["ephemeral_check_period"])
+
 
 # wrap process and run this concurrently indefinitely forever and ever :P
 def process_audio():
@@ -97,18 +140,21 @@ def process_audio():
         audio_file[uuid]["status"] = "processed"
         # put try catch block here so the thread wont fail
         try:
+            # raise ValueError("manually raised failed transcribtion")
             # this function call return transcribtion as dict and write it to disk
             # TODO! file need to be purged periodically or once the task is finished
             output = whisper_to_vtt(
-                model=model, 
-                audio_file_path=file_path, 
-                output_dir=config["output_vtt_dir"], 
+                model=model,
+                audio_file_path=file_path,
+                output_dir=config["output_vtt_dir"],
                 save_output_as_file=config["upload_to_bucket"],
             )
-            # upload all vtt to s3 bucket 
+            # upload all vtt to s3 bucket
             if config["upload_to_bucket"]:
                 for upload_file_path in output["list_file_path"]:
                     upload_to_bucket(upload_file_path)
+                # store this in dict so purging script can get the file easily
+                audio_file[uuid]["list_file_path"] = output["list_file_path"]
 
             # store transcribed output
             audio_file[uuid]["status"] = "transcribed"
@@ -118,13 +164,16 @@ def process_audio():
             print(output)
             # store error message reason
             audio_file[uuid]["output_as_text"] = output
-            audio_file[uuid]["status"] = "failed to transcribe audio"   
+            audio_file[uuid]["status"] = "failed to transcribe audio"
         print()
 
 
 process_audio_thread = Thread(target=process_audio)
+purging_thread = Thread(target=purge_data_periodially)
 
 process_audio_thread.start()
+purging_thread.start()
+
 
 @app.get("/testing/{name}")
 async def test_connection(name: str):
@@ -257,6 +306,7 @@ async def upload_audio_to_queue_process(file: UploadFile = File(...)) -> dict:
 
     return {"queue_id": uuid_name, "status": audio_file[uuid_name]["status"]}
 
+
 @app.get("/transcribtion_status/{uuid_name}")
 async def transcribtion_status(uuid_name: str):
     """gets queue info"""
@@ -268,10 +318,14 @@ async def transcribtion_status(uuid_name: str):
         except KeyError:
             raise HTTPException(status_code=404, detail="UUID not found")
 
+
 @app.get("/queue_length")
 async def queue_length():
     """gets queue length"""
-    return {"queue_length": internal_queue.qsize(), "max__queue_length": config["maximum_queue"]}
+    return {
+        "queue_length": internal_queue.qsize(),
+        "max__queue_length": config["maximum_queue"],
+    }
 
 
 @app.post("/upload/audio_test_preprocessing/")
